fc954d4a75cf1688c39b720a432c717c4ec1546b
komarios <komarios@yahoo.com>
2017-11-29 11:21:46 +0200

    Update WebSocketEntryPoint.java

--- marso/controller/websocket/WebSocketEntryPoint.java
+++ marso/controller/websocket/WebSocketEntryPoint.java
@@ -14,33 +14,11 @@ import java.util.concurrent.ConcurrentHashMap;
 @Component
 public class WebSocketEntryPoint extends TextWebSocketHandler {
 	private static final Logger logger = Logger.getLogger(WebSocketEntryPoint.class);
-	
-	private void handleInvalidMessage( WebSocketSession session, PCMessage pcmessage )
-			throws InterruptedException, IOException{
-		session.sendMessage( new TextMessage( "Invalid Message:"+pcmessage.getMessage() ) );
-	}
 
 	@Override
 	public void handleTextMessage(WebSocketSession session, TextMessage textMessage)
 			throws InterruptedException, IOException {
-		PCMessage pcmessage = null;
-		try{
-			pcmessage = new PCMessage( textMessage );
-			pcmessage.logMessage();
-			if ( pcmessage.parseMessage() )
-				handlePCMessage( session, pcmessage );
-			else
-				handleInvalidMessage( session, pcmessage );
-		} catch (InterruptedException e){
-			logger.error("InterruptedException:", e);
-			throw e;
-		} catch (IOException e){
-			logger.error("IOException:", e);
-			throw e;
-		} catch (Exception e){
-			logger.error("Exception:", e);
-			session.sendMessage( new TextMessage( "Exception:" + e.getMessage() ) );
-		}
+		handleTextMessage( session, textMessage );
 	}
 
 	@Override
@@ -51,14 +29,6 @@ public class WebSocketEntryPoint extends TextWebSocketHandler {
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
 		logger.info( "WebSocket was closed:"+ session.getId() );
-		String user_id = reverseSessionList.get( session.getId() );
-		if ( user_id != null ) {
-			if( sessionList.get(user_id) != null )
-				sessionList.remove(user_id);
-			if( gameStartList.get(user_id) != null )
-				gameStartList.remove(user_id);
-			reverseSessionList.remove(session.getId());
-			logger.info( "WebSocket was cleaned up:"+user_id );
-		}
+		cleanUpOnDisconnect( session, status);
 	}
 }
