f9d41b09c9e5fede0318c074502bbb5da28a696d
komarios <komarios@yahoo.com>
2017-11-29 14:48:31 +0200

    Update GameReady.java

--- marso/controller/websocket/GameReady.java
+++ marso/controller/websocket/GameReady.java
@@ -1,6 +1,6 @@
 package marso.controller.websocket;
 
-protected class GameReady extends GameAction {
+protected class GameReady implements GameAction {
           public void execute( WebSocketSession session, PCMessage pcmessage, WebSocketSession oppoSession )
 			throws InterruptedException, IOException{
 		if( oppoSession == null )
