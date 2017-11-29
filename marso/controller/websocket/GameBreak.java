a4130a11d7ff746dd5f0f7f88604295c7a313416
komarios <komarios@yahoo.com>
2017-11-29 13:40:19 +0200

    Create GameBreak.java

---
+++ marso/controller/websocket/GameBreak.java
@@ -0,0 +1,18 @@
+package marso.controller.websocket;
+
+protected class GameBreak extends GameAction {
+          public void execute( WebSocketSession session, PCMessage pcmessage )
+			throws InterruptedException, IOException{			
+		WebSocketSession oppoSession = sessionList.get( pcmessage.getOppoId() );
+		if( oppoSession == null )
+			sendMsgToClient( session, "oppo_no_conn" );
+		else {
+			gameStartList.put( pcmessage.getUserId(), "ready");
+			sendMsgToClient( session,  "readyok" );
+			if ( gameStartList.get( pcmessage.getOppoId() ) != null ) {
+				sendMsgToClient( session,  "begin:7" );
+				sendMsgToClient( oppoSession, "begin:7" );
+			}
+		}
+          }
+}
