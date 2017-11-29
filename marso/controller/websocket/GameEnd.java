7611a604387e10bbf94458ea7d2fb86bc87cb122
komarios <komarios@yahoo.com>
2017-11-29 13:45:57 +0200

    Create GameEnd.java

---
+++ marso/controller/websocket/GameEnd.java
@@ -0,0 +1,15 @@
+package marso.controller.websocket;
+
+protected class GameEnd extends GameAction {
+          public void execute( WebSocketSession session, PCMessage pcmessage )
+			throws InterruptedException, IOException{			
+		WebSocketSession oppoSession = sessionList.get( pcmessage.getOppoId() );
+		if( oppoSession == null )
+			sendMsgToClient( session, "oppo_no_conn" );
+		else if( gameStartList.get( pcmessage.getOppoId() ) == null )
+			sendMsgToClient( session, "oppo_no_ready" );
+		else
+			sendMsgToClient( oppoSession, pcmessage.getAction() );
+		//TODO: ACK: session.sendMessage( new TextMessage( "breakok:moveid" ) );
+          }
+}
