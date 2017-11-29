5565380e57b8d9f51e49439adb7748aeda05bb34
komarios <komarios@yahoo.com>
2017-11-29 14:41:33 +0200

    Update GamePing.java

--- marso/controller/websocket/GamePing.java
+++ marso/controller/websocket/GamePing.java
@@ -1,7 +1,7 @@
 package marso.controller.websocket;
 
 protected class GamePing extends GameAction {
-          public void execute( WebSocketSession session, PCMessage pcmessage )
+          public void execute( WebSocketSession session, PCMessage pcmessage, WebSocketSession oppoSession )
 			throws InterruptedException, IOException{			
 		sendMsgToClient( session, "pong:"+pcmessage.getData() );
 		//TODO: Handle ping from user-to-user 
