a2c2dea3e9a8f8dbf867e5925fec7c472332634b
komarios <komarios@yahoo.com>
2017-11-29 13:44:35 +0200

    Update GamePing.java

--- marso/controller/websocket/GamePing.java
+++ marso/controller/websocket/GamePing.java
@@ -3,13 +3,7 @@ package marso.controller.websocket;
 protected class GamePing extends GameAction {
           public void execute( WebSocketSession session, PCMessage pcmessage )
 			throws InterruptedException, IOException{			
-		WebSocketSession oppoSession = sessionList.get( pcmessage.getOppoId() );
-		if( oppoSession == null )
-			sendMsgToClient( session, "oppo_no_conn" );
-		else if( gameStartList.get( pcmessage.getOppoId() ) == null )
-			sendMsgToClient( session, "oppo_no_ready" );
-		else
-			sendMsgToClient( oppoSession, pcmessage.getAction() );
-		//TODO: ACK: session.sendMessage( new TextMessage( "breakok:moveid" ) );
+		sendMsgToClient( session, "pong:"+pcmessage.getData() );
+		//TODO: Handle ping from user-to-user 
           }
 }
