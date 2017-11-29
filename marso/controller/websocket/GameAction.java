2ed7def0c3010b703d318b10d45d89e58eb53e47
komarios <komarios@yahoo.com>
2017-11-29 14:46:07 +0200

    Update GameAction.java

--- marso/controller/websocket/GameAction.java
+++ marso/controller/websocket/GameAction.java
@@ -1,5 +1,7 @@
 package marso.controller.websocket;
 
+import org.springframework.web.socket.WebSocketSession;
+
 public interface GameAction {
          public void execute( WebSocketSession session, PCMessage pcmessage, WebSocketSession oppoSession );
 }
