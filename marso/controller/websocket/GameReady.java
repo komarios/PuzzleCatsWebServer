1d0f7ecb623e95ea08481b81b578c6853240aec7
komarios <komarios@yahoo.com>
2017-11-29 13:39:35 +0200

    Update GameReady.java

--- marso/controller/websocket/GameReady.java
+++ marso/controller/websocket/GameReady.java
@@ -4,13 +4,16 @@ package marso.controller.websocket;
 protected class GameReady extends GameAction {
           public void execute( WebSocketSession session, PCMessage pcmessage )
 			throws InterruptedException, IOException{			
-		sessionList.put( pcmessage.getUserId(), session);
-		reverseSessionList.put( session.getId(), pcmessage.getUserId() );
-		sendMsgToClient( session, "connok" );
-		if ( sessionList.get( pcmessage.getOppoId() ) != null ) {
-			sendMsgToClient( session, "conn2ok" );
-			WebSocketSession oppoSession = sessionList.get( pcmessage.getOppoId() );
-			sendMsgToClient( oppoSession, "conn2ok" );
+		WebSocketSession oppoSession = sessionList.get( pcmessage.getOppoId() );
+		if( oppoSession == null )
+			sendMsgToClient( session, "oppo_no_conn" );
+		else {
+			gameStartList.put( pcmessage.getUserId(), "ready");
+			sendMsgToClient( session,  "readyok" );
+			if ( gameStartList.get( pcmessage.getOppoId() ) != null ) {
+				sendMsgToClient( session,  "begin:7" );
+				sendMsgToClient( oppoSession, "begin:7" );
+			}
 		}
           }
 }
