53ec13748e65e66512512c9263e7401d90f06a50
komarios <komarios@yahoo.com>
2017-11-29 17:19:16 +0200

    Update GameConnect.java

--- marso/controller/websocket/GameConnect.java
+++ marso/controller/websocket/GameConnect.java
@@ -3,12 +3,9 @@ package marso.controller.websocket;
 protected class GameConnect implements GameAction {
           public void execute( WebSocketSession session, PCMessage pcmessage, WebSocketSession oppoSession )
 			throws InterruptedException, IOException{			
-		sessionList.put( pcmessage.getUserId(), session);
-		reverseSessionList.put( session.getId(), pcmessage.getUserId() );
 		sendMsgToClient( session, "connok" );
 		if ( sessionList.get( pcmessage.getOppoId() ) != null ) {
 			sendMsgToClient( session, "conn2ok" );
-			WebSocketSession oppoSession = sessionList.get( pcmessage.getOppoId() );
 			sendMsgToClient( oppoSession, "conn2ok" );
 		}
           }
