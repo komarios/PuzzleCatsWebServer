11367881564d6ad56ddc145204d0ffaa8c432e53
komarios <komarios@yahoo.com>
2017-11-30 10:58:23 +0200

    Update GameConnect.java

--- marso/controller/websocket/GameConnect.java
+++ marso/controller/websocket/GameConnect.java
@@ -4,10 +4,10 @@ protected class GameConnect implements GameAction {
           public List<String[]> execute( String mySessionId, PCMessage pcmessage, String oppoSessionId )
 			throws InterruptedException, IOException{			
 		List<String[]> messages = new ArrayList<String[]>();
-		messages.add( new String[] { mySessionId, "connok" );
+		messages.add( new String[] { mySessionId, "connok" } );
 		if ( ! oppoSessionId.equals("") ) {
-			messages.add( new String[] { mySessionId, "conn2ok" );
-			messages.add( new String[] { oppoSessionId, "conn2ok" );
+			messages.add( new String[] { mySessionId, "conn2ok" } );
+			messages.add( new String[] { oppoSessionId, "conn2ok" } );
 		}
 		return messages;
           }
