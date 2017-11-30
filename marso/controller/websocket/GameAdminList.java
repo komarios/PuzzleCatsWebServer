9958350656c26b3be023017cc4d32cf4b6a68165
komarios <komarios@yahoo.com>
2017-11-30 12:03:47 +0200

    Update GameAdminList.java

--- marso/controller/websocket/GameAdminList.java
+++ marso/controller/websocket/GameAdminList.java
@@ -1,12 +1,11 @@
 package marso.controller.websocket;
 
 protected class GameAdminList implements GameAction {
-          public List<String[]> execute( String mySessionId, PCMessage pcmessage, String oppoSessionId )
-			throws InterruptedException, IOException{
-		List<String[]> messages = new ArrayList<String[]>();			
-		messages.add( new String[] { mySessionId, "sessionList:"+sessionList } );
-		messages.add( new String[] { mySessionId, "reverseSessionList:"+reverseSessionList } );
-		messages.add( new String[] { mySessionId, "gameStartList:"+gameStartList } );
-		return messages;
+          public PCResponse execute( String mySessionId, PCMessage pcmessage, String oppoSessionId ) {
+		PCResponse response = new PCResponse();	
+		response.addMessage( mySessionId, "sessionList:"+sessionList );
+		response.addMessage( mySessionId, "reverseSessionList:"+reverseSessionList );
+		response.addMessage( mySessionId, "gameStartList:"+gameStartList );
+		return response;
           }
 }
