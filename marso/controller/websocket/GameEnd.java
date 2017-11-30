d3dcbab2ec9e6f92dee283d2b63d478039045d8b
komarios <komarios@yahoo.com>
2017-11-30 11:58:58 +0200

    Update GameEnd.java

--- marso/controller/websocket/GameEnd.java
+++ marso/controller/websocket/GameEnd.java
@@ -1,8 +1,7 @@
 package marso.controller.websocket;
 
 protected class GameEnd implements GameAction {
-          public PCResponse execute( String mySessionId, PCMessage pcmessage, String oppoSessionId )
-			throws InterruptedException, IOException{			
+          public PCResponse execute( String mySessionId, PCMessage pcmessage, String oppoSessionId ) {			
 		PCResponse response = new PCResponse();
 		if( oppoSessionId.equals("") )
 			response.addMessage( mySessionId, "oppo_no_conn" );
