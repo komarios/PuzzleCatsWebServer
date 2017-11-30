2b10f465d2f89786250456a7b89cfa9e5197c705
komarios <komarios@yahoo.com>
2017-11-30 11:50:59 +0200

    Create PCResponse.java

---
+++ marso/controller/websocket/PCResponse.java
@@ -0,0 +1,11 @@
+package marso.controller.websocket;
+
+public class PCResponse {
+  List<String[]> messages = new ArrayList<String[]>();
+  public void addMessage(String userId, String msg){
+    messages.add( new String[] { userId, msg } );
+  }
+  public String[] getMessage(int i){
+    return messages.get( i );
+  }
+}
