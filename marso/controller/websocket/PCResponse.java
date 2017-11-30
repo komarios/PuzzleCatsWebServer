c52c2a4c30d26b62e074e4f6bb7a051003c2a95c
komarios <komarios@yahoo.com>
2017-11-30 12:06:36 +0200

    Update PCResponse.java

--- marso/controller/websocket/PCResponse.java
+++ marso/controller/websocket/PCResponse.java
@@ -1,11 +1,8 @@
 package marso.controller.websocket;
 
-public class PCResponse {
-        List<String[]> messages = new ArrayList<String[]>();
+protected class PCResponse {
+        protected List<String[]> messages = new ArrayList<String[]>();
         public void addMessage(String userId, String msg){
                 messages.add( new String[] { userId, msg } );
-        }
-        public List<String[]> getMessages(){
-                return messages;
         }
 }
