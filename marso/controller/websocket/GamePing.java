ed4f7e83776125ebcf51972a81b5714032a04cf3
komarios <komarios@yahoo.com>
2017-11-30 12:00:57 +0200

    Update GamePing.java

--- marso/controller/websocket/GamePing.java
+++ marso/controller/websocket/GamePing.java
@@ -1,10 +1,10 @@
 package marso.controller.websocket;
 
 protected class GamePing implements GameAction {
-          public PCResponse execute( String mySessionId, PCMessage pcmessage, String oppoSessionId ) {			
+	public PCResponse execute( String mySessionId, PCMessage pcmessage, String oppoSessionId ) {			
 		PCResponse response = new PCResponse();
 		response.addMessage( mySessionId, "pong:"+pcmessage.getData() );
 		//TODO: Handle ping from user-to-user 
 		return response;
-          }
+	}
 }
