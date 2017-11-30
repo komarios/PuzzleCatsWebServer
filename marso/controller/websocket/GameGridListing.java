2d6612870434f1d2098a5c6267474c0c07c078b7
komarios <komarios@yahoo.com>
2017-11-30 10:57:38 +0200

    Update GameGridListing.java

--- marso/controller/websocket/GameGridListing.java
+++ marso/controller/websocket/GameGridListing.java
@@ -1,11 +1,13 @@
 package marso.controller.websocket;
 
 protected class GameGridListing implements GameAction {
-          public void execute( WebSocketSession session, PCMessage pcmessage, WebSocketSession oppoSession )
+          public List<String[]> execute( String mySessionId, PCMessage pcmessage, String oppoSessionId )
 			throws InterruptedException, IOException{			
-		if( oppoSession == null )
-			sendMsgToClient( session, "oppo_no_conn" );
+		List<String[]> messages = new ArrayList<String[]>();
+		if( oppoSessionId.equals("") )
+			messages.add( new String[] { mySessionId, "oppo_no_conn" } );
 		else
-			sendMsgToClient( oppoSession, pcmessage.getAction() );
+			messages.add( new String[] { oppoSessionId, pcmessage.getAction() } );
+		return messages;
           }
 }
