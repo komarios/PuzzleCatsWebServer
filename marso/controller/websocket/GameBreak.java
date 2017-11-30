996f3b4bedaf3e09706a3f4e4b0b5e15ae2330a8
komarios <komarios@yahoo.com>
2017-11-30 11:56:08 +0200

    Update GameBreak.java

--- marso/controller/websocket/GameBreak.java
+++ marso/controller/websocket/GameBreak.java
@@ -1,16 +1,16 @@
 package marso.controller.websocket;
 
 protected class GameBreak implements GameAction {
-	  public List<String[]> execute( String mySessionId, PCMessage pcmessage, String oppoSessionId )
+	  public PCResponse  execute( String mySessionId, PCMessage pcmessage, String oppoSessionId )
 			throws InterruptedException, IOException{
-		List<String[]> messages = new ArrayList<String[]>();
+		PCResponse response = new PCResponse();
 		if( oppoSessionId.equals("") )
-			messages.add( new String[] { mySessionId, "oppo_no_conn" } );
+			response.addMessage( mySessionId, "oppo_no_conn" );
 		else if( gameStartList.get( pcmessage.getOppoId() ) == null )
-			messages.add( new String[] { mySessionId, "oppo_no_ready" } );
+			response.addMessage( mySessionId, "oppo_no_ready" );
 		else
-			messages.add( new String[] { oppoSessionId, pcmessage.getAction() } );
+			response.addMessage( oppoSessionId, pcmessage.getAction() );
 		//TODO: ACK: session.sendMessage( new TextMessage( "breakok:moveid" ) );
-		return messages;
+		return response;
           }
 }
