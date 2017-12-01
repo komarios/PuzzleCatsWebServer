dbc5b4c6e90bcf45cd180a562a8984381ff5c376
komarios <komarios@yahoo.com>
2017-12-01 17:10:27 +0200

    Update GameInvalidAction.java

--- marso/controller/websocket/GameInvalidAction.java
+++ marso/controller/websocket/GameInvalidAction.java
@@ -1,6 +1,6 @@
 package marso.controller.websocket;
 
-protected class GameInvalidAction implements GameAction {
+public class GameInvalidAction implements GameAction {
 	  public PCResponse execute( PCMessage pcmessage, String oppoStatus ) {
 		PCResponse response = new PCResponse();
 		response.addMessage( pcmessage.getUserId(), "Invalid Action" );
