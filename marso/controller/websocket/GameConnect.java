455393fd08f48d6915f34072b0e36bad1de099de
komarios <komarios@yahoo.com>
2017-12-01 17:09:36 +0200

    Update GameConnect.java

--- marso/controller/websocket/GameConnect.java
+++ marso/controller/websocket/GameConnect.java
@@ -1,6 +1,6 @@
 package marso.controller.websocket;
 
-protected class GameConnect implements GameAction {
+public class GameConnect implements GameAction {
           public PCResponse execute( PCMessage pcmessage, String oppoStatus ) {			
 		PCResponse response = new PCResponse();
 		response.addMessage( pcmessage.getUserId(), "connok" );
